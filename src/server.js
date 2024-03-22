77fc9442a097f0ecd5bc2ab6d0eca59ae1845f77
ThanhNeNha2 <ta.coaching.v2@gmail.com>
2024-03-22 21:27:19 +0700

    node va sequelize

---
+++ src/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import bodyParser from "body-parser";
+import viewEngine from "./config/viewEngine";
+import initWebRoutes from "./route/web";
+require("dotenv").config();
+let app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+viewEngine(app);
+initWebRoutes(app);
+let port = process.env.PORT || 8888;
+app.listen(port, () => {
+  console.log("Backend nodejs is port " + port);
+});
