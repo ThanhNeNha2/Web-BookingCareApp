add074d26a95ec5a1535632bc0e6ac28f59e2374
ThanhNeNha2 <ta.coaching.v2@gmail.com>
2024-05-02 18:21:22 +0700

    50 hoan thien giao dien clone

--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -1,14 +1,28 @@
 import React, { Component } from "react";
-import { Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import HomeHeader from "./HomeHeader";
 import Specialty from "./Section/Specialty";
+import MedicalFacility from "./Section/MedicalFacility";
+import OutStandingDoctor from "./Section/OutStandingDoctor";
+import HandBook from "./Section/HandBook";
+import "./HomePage.scss";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 class HomePage extends Component {
   render() {
+    let settings = {
+      infinite: true,
+      speed: 500,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+    };
     return (
       <div>
         <HomeHeader />
-        <Specialty />
+        <Specialty settings={settings} />
+        <MedicalFacility settings={settings} />
+        <OutStandingDoctor settings={settings} />
+        <HandBook settings={settings} />
       </div>
     );
   }
--- src/containers/HomePage/Section/HandBook.js
+++ src/containers/HomePage/Section/HandBook.js
@@ -1,49 +1,49 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import "./Specialty.scss";
+
 import { FormattedMessage } from "react-intl";
 // Import css files
 import Slider from "react-slick";
 
-class Specialty extends Component {
+class HandBook extends Component {
   render() {
     return (
-      <div className="section-share section-specialty">
+      <div className="section-share section-handbook">
         <div className="section-container">
           <div className="section-header">
-            <span className="title-section">Chuyên khoa phổ biến </span>
+            <span className="title-section">Cơ sở ý tế nổi bật </span>
             <button className="btn-section"> Xem thêm </button>
           </div>
           <div className="section-body">
             <Slider {...this.props.settings}>
               <div className="section-customize">
-                <div className="bg-image section-specialty" />
-                <div> Cơ xương khớp 1</div>
+                <div className="bg-image section-handbook" />
+                <div> Hệ thống Y tế Thu Cúc 1</div>
               </div>
 
               <div className="section-customize">
-                <div className="bg-image section-specialty" />
-                <div> Cơ xương khớp 2</div>
+                <div className="bg-image section-handbook" />
+                <div> Hệ thống Y tế Thu Cúc 2</div>
               </div>
 
               <div className="section-customize">
-                <div className="bg-image section-specialty" />
-                <div> Cơ xương khớp 3</div>
+                <div className="bg-image section-handbook" />
+                <div> Hệ thống Y tế Thu Cúc 3</div>
               </div>
 
               <div className="section-customize">
-                <div className="bg-image section-specialty" />
-                <div> Cơ xương khớp 4</div>
+                <div className="bg-image section-handbook" />
+                <div> Hệ thống Y tế Thu Cúc 4</div>
               </div>
 
               <div className="section-customize">
-                <div className="bg-image section-specialty" />
-                <div> Cơ xương khớp 5</div>
+                <div className="bg-image section-handbook" />
+                <div> Hệ thống Y tế Thu Cúc 5</div>
               </div>
 
               <div className="section-customize">
-                <div className="bg-image section-specialty" />
-                <div> Cơ xương khớp 6</div>
+                <div className="bg-image section-handbook" />
+                <div> Hệ thống Y tế Thu Cúc 6</div>
               </div>
             </Slider>
           </div>
@@ -64,4 +64,4 @@ const mapDispatchToProps = (dispatch) => {
   return {};
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Specialty);
+export default connect(mapStateToProps, mapDispatchToProps)(HandBook);
