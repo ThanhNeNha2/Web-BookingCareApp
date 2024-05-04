58bc831035bebbb9fd3be08e6e15f5702defadae
ThanhNeNha2 <ta.coaching.v2@gmail.com>
2024-05-04 20:37:09 +0700

    57 thuc hanh reddux

--- src/services/userService.js
+++ src/services/userService.js
@@ -21,10 +21,15 @@ const deleteUserService = (userId) => {
 const editUserService = (inputData) => {
   return axios.put("/api/edit-user", inputData);
 };
+
+const getAllCodeService = (inputType) => {
+  return axios.get(`/api/allcode?type=${inputType}`);
+};
 export {
   handleLoginApi,
   getALLUsers,
   createNewUserService,
   deleteUserService,
   editUserService,
+  getAllCodeService,
 };
--- src/store/reducers/adminReducer.js
+++ src/store/reducers/adminReducer.js
@@ -1,33 +1,34 @@
-import actionTypes from '../actions/actionTypes';
+import actionTypes from "../actions/actionTypes";
 
 const initialState = {
-    isLoggedIn: false,
-    adminInfo: null
-}
+  genders: [],
+  roles: [],
+  positions: [],
+};
 
-const appReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case actionTypes.ADMIN_LOGIN_SUCCESS:
-            return {
-                ...state,
-                isLoggedIn: true,
-                adminInfo: action.adminInfo
-            }
-        case actionTypes.ADMIN_LOGIN_FAIL:
-            return {
-                ...state,
-                isLoggedIn: false,
-                adminInfo: null
-            }
-        case actionTypes.PROCESS_LOGOUT:
-            return {
-                ...state,
-                isLoggedIn: false,
-                adminInfo: null
-            }
-        default:
-            return state;
-    }
-}
+const adminReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.FETCH_GENDER_START:
+      console.log("bat dau");
+      return {
+        ...state,
+      };
+    case actionTypes.FETCH_GENDER_SUCCESS:
+      let copyState = { ...state };
+      copyState.genders = action.data;
 
-export default appReducer;
+      console.log("thanh cong", copyState);
+      return {
+        ...copyState,
+      };
+    case actionTypes.FETCH_GENDER_FAIlED:
+      console.log("ket thuc ");
+      return {
+        ...state,
+      };
+    default:
+      return state;
+  }
+};
+
+export default adminReducer;
