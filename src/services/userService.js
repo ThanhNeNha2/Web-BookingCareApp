4c681ac4f113f576bd03d8b33ef02d198f9544ac
ThanhNeNha2 <ta.coaching.v2@gmail.com>
2024-05-06 23:34:37 +0700

    61 1 it node crud

--- src/services/userService.js
+++ src/services/userService.js
@@ -153,7 +153,7 @@ let deleteUser = (id) => {
 let updateUser = (data) => {
   return new Promise(async (resolve, reject) => {
     try {
-      if (!data.id) {
+      if (!data.id || !data.roleId || !data.positionId || !data.gender) {
         resolve({
           errCode: 2,
           errMessage: "Missing required parameter Id",
@@ -168,6 +168,10 @@ let updateUser = (data) => {
           firstName: data.firstName,
           lastName: data.lastName,
           address: data.address,
+          roleId: data.roleId,
+          positionId: data.positionId,
+          gender: data.gender,
+          phonenumber: data.phonenumber,
         });
         await user.save();
         resolve({
