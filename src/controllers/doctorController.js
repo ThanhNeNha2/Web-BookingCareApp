1ea02cb5b0910975d60591fdd07b2ace13ec953e
ThanhNeNha2 <ta.coaching.v2@gmail.com>
2024-05-11 20:26:06 +0700

    67

--- src/controllers/doctorController.js
+++ src/controllers/doctorController.js
@@ -15,4 +15,29 @@ let getTopDoctorHome = async (req, res) => {
   }
 };
 
-module.exports = { getTopDoctorHome };
+let getAllDoctors = async (req, res) => {
+  try {
+    let doctors = await doctorService.getAllDoctors();
+    return res.status(200).json(doctors);
+  } catch (e) {
+    console.log(e);
+    return res.status(200).json({
+      errCode: -1,
+      message: "Error from server....",
+    });
+  }
+};
+let postInforDoctor = async (req, res) => {
+  try {
+    let response = await doctorService.saveDetailInforDoctor(req.body);
+
+    return res.status(200).json(response);
+  } catch (e) {
+    console.log(e);
+    return res.status(200).json({
+      errCode: -1,
+      message: "Error from server....",
+    });
+  }
+};
+module.exports = { getTopDoctorHome, getAllDoctors, postInforDoctor };
