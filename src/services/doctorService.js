a5faacee4638b169678e10f276a2a7ddfae09f4e
ThanhNeNha2 <ta.coaching.v2@gmail.com>
2024-05-16 21:25:48 +0700

    74 bulkCreate

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -1,7 +1,7 @@
-import { where } from "sequelize";
 import db from "../models/index";
-import { raw } from "body-parser";
-
+require("dotenv").config();
+import _ from "lodash";
+const MAX_NUMBER_SCHEDULE = process.env.MAX_NUMBER_SCHEDULE;
 let getTopDoctorHome = (limitInput) => {
   return new Promise(async (resolve, reject) => {
     try {
@@ -142,9 +142,57 @@ let getDetailDoctorByIds = (inputId) => {
     }
   });
 };
+
+let bulkCreateSchedule = (data) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      if (!data.arrSchedule || !data.doctorId || !data.formateDate) {
+        resolve({
+          errCode: 1,
+          errMessage: "Missing required param!",
+        });
+      } else {
+        let schedule = data.arrSchedule;
+        if (schedule && schedule.length > 0) {
+          schedule = schedule.map((item) => {
+            item.maxNumber = MAX_NUMBER_SCHEDULE;
+            return item;
+          });
+        }
+
+        // kiểm tra xem đã có trong data chưa rồi bỏ ra ( rất khó )
+        let existing = await db.Schedule.findAll({
+          where: { doctorId: data.doctorId, date: data.formateDate },
+          attributes: ["timeType", "date", "doctorId", "maxNumber"],
+          raw: true,
+        });
+        if (existing && existing.length > 0) {
+          existing = existing.map((item) => {
+            item.date = new Date(item.date).getTime();
+            return item;
+          });
+        }
+        let toCreate = _.differenceWith(schedule, existing, (a, b) => {
+          return a.timeType === b.timeType && a.date === b.date;
+        });
+        if (toCreate && toCreate.length > 0) {
+          await db.Schedule.bulkCreate(toCreate);
+        }
+
+        resolve({
+          errCode: 0,
+          data,
+        });
+      }
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
 module.exports = {
   getTopDoctorHome,
   getAllDoctors,
   saveDetailInforDoctor,
   getDetailDoctorByIds,
+  bulkCreateSchedule,
 };
