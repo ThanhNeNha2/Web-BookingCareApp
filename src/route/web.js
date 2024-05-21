1dd4560ed6066ee6374fe66c5985ad49938a2d53
ThanhNeNha2 <ta.coaching.v2@gmail.com>
2024-05-21 17:25:46 +0700

    83 api load dong thong tin bs

--- src/route/web.js
+++ src/route/web.js
@@ -41,6 +41,10 @@ let initWebRoutes = (app) => {
     "/api/get-etra-infor-doctor-by-id",
     doctorController.getExtraInforDoctorById
   );
+  router.get(
+    "/api/get-profile-doctor-by-id",
+    doctorController.getProfileDoctorById
+  );
   return app.use("/", router);
 };
 module.exports = initWebRoutes;
